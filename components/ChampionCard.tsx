e64fc545f22924b2e7516ff88b3a168a594a6280
nico-mayer <nico.m94@icloud.com>
2022-04-21 17:12:07 +0200

    first commit

---
+++ components/ChampionCard.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+
+interface Props {
+  champion: {
+    alias: string
+    id: number
+    name: string
+    roles: string[]
+    squarePortraitPath: string
+  }
+  rerollFunc: (event: React.MouseEvent<HTMLButtonElement>) => void
+  index: number
+}
+
+function ChampionCard({ champion, index, rerollFunc }: Props) {
+  return (
+    <div className=" m-2 flex h-96 w-80 flex-col bg-white">
+      <img
+        className="flex flex-1 object-cover"
+        src={`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/${champion?.id}/${champion?.id}000.jpg`}
+      />
+      <div className="flex">
+        {champion?.name} {champion?.roles[0]}
+      </div>
+      <button className="bg-red-100" onClick={() => rerollFunc(index)}>
+        Reroll
+      </button>
+    </div>
+  )
+}
+
+export default ChampionCard
---
+++ utils/populateIndexArray.ts
@@ -0,0 +1,13 @@
+function populateIndexArray(size: number, maxIndex: number) {
+  const indexArray: number[] = []
+
+  while (indexArray.length < size) {
+    const randomIndex = Math.floor(Math.random() * maxIndex)
+    if (!indexArray.includes(randomIndex) && randomIndex !== 0) {
+      indexArray.push(randomIndex)
+    }
+  }
+  return indexArray
+}
+
+export default populateIndexArray
